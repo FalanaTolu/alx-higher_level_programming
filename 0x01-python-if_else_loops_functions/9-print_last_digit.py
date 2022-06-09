0dad0030ea0a311abb579baeb9b1ab4ad1dd29a6
Falana Tolu <falanaft@gmail.com>
2022-06-09 11:05:04 -0700

    Update 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,6 +2,6 @@
 def print_last_digit(number):
     if number < 0:
         number = -number
-        last_dig = number % 10
-        print(last_dig, end="")
-        return last_dig
+    last_dig = number % 10
+    print(last_dig, end="")
+    return last_dig
